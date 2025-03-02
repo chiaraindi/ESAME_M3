8da5a1cdf36e2aa3317483f800fd52023fd59a08
Chiara Indirli <chiaraindirli2@gmail.com>
2025-03-02 09:50:55 +0100

    desktop finito

--- Esame/script.js
+++
@@ -1,8 +0,0 @@
-let index = 0;
-function moveCarousel(direction) {
-    const track = document.querySelector(".carousel-track");
-    const cards = document.querySelectorAll(".card");
-    const cardWidth = cards[0].offsetWidth + 10;
-    index = Math.max(0, Math.min(index + direction, cards.length - 6));
-    track.style.transform = `translateX(-${index * cardWidth}px)`;
-}
