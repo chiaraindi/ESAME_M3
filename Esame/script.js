dfa75073ac7cb7e5e56d0b9c2da90deff1b8c9a3
Chiara Indirli <chiaraindirli2@gmail.com>
2025-03-08 18:39:54 +0100

    nav bar ok

---
+++ Esame/script.js
@@ -0,0 +1,10 @@
+/*Scroll Navbar */
+
+    window.addEventListener("scroll", function() {
+        let navbar = document.querySelector(".navbar");
+        if (window.scrollY > 50) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
